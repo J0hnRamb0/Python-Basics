89e32b04a49da46189895b686c89afcfe4f54c5a
J0hnRamb0 <76633596+J0hnRamb0@users.noreply.github.com>
2025-02-20 18:38:51 +0100

    Add files via upload

---
+++ functions_module.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append("C:/Users/Jonathon/python app")
+
+import calculator_using_functions as calculator
+
+# result1 = calculator.addition()
+# result2 = calculator.subtraction(113, 12)
+# result3 = calculator.multiplication(13, 12)
+# result4 = calculator.division(130, 12)
+# result5 = calculator.quotient(130, 12)
+# result6 = calculator.remainder(130, 12)
+
+# print(f"Addition result is : {result1}")
